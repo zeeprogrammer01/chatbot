8867506fbf34b60e5b635d16ded1a4d76b451e51
zeeprogrammer01 <76761941+zeeprogrammer01@users.noreply.github.com>
2021-03-25 13:10:14 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+import sqlite3
+
+
+def respond(message):
+    if message in messages:
+        bot_message = random.choice(messages[message])
+    else:
+        bot_message = random.choice(messages["default"])
+    return bot_message
+
+
+conn = sqlite3.connect('chatbot.db')
+
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE chatMessages(
+# input text,
+# response text
+
+# )""")
+
+messages = [('Hello', 'Hi I am Chatbot!'),
+            ('How are you?', 'I am good'),
+            ('How is the weather today?', 'It is really cold today'),
+            ('What can you do?', 'I can give you tips on PCOS'),
+            ('Bye', 'Thanks for your time')]
+
+c.executemany('INSERT INTO chatMessages VALUES(?,?);', messages)
+
+c.execute("SELECT * FROM chatMessages ")
+
+print(c.fetchall())
+conn.commit()
+
+conn.close()
